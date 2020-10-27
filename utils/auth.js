5f3de7d5553e73bdff8ddf9b3a5e960caafaaa71
Wren McPherson <wren.mcpherson@yahoo.com>
2020-10-27 19:45:45 -0400

    working on backend and user auth, made auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -4,9 +4,39 @@ const jwt = require('jsonwebtoken');
 const { User } = require('../db/models');
 const { jwtConfig: { secret, expiresIn } } = require('../config');
 
+const getUSerToken = (user) => {
+    const userData = {
+        id: user.id, email: user.email
+    }
+    const token = jwt.sign( { data: userData }, 
+        secret, 
+        { expiresIn: parseInt(expiresIn, 10) }
+    )
+    return token;
+}
+
 const restoreUser = (req, res, next) => {
     const { token } = req;
     if (!token) {
-        return res.set('WWW-')
-    }
+        return res.set('WWW-Authenticate', 'Bearer').status(401).end();
+    };
+    return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+        if (err) {
+            err.status = 401;
+            return next(err)
+        }
+        const { id } = jwtPayload.data;
+        try {
+            req.user = await User.findByPk(id)
+        } catch (e) {
+            return next(e);
+        }
+        if (!req.user) {
+            return res.set('WWW-Authenticate', 'Bearer').status(401).end();
+        }
+        return next();
+    })
 }
+const requireAuth = [bearerToken(), restoreUser];
+
+module.exports = { getUserToken, requireAuth };
