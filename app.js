929b327a7ccf07ec9e61c6b31cf2652a65fc703d
Wren McPherson <wren.mcpherson@yahoo.com>
2020-10-28 12:20:06 -0400

    updated app.js

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/api/users');
 const postsRouter = require('./routes/api/posts');
-
+const { environment } = require('./config');
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000" }))
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(indexRouter);
 app.use(usersRouter);
 app.use(postsRouter);
+
+
+// error handling
+
 
 module.exports = app;
