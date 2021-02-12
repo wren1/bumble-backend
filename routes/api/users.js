a0b2c1d2338ad054b5a230715578c55756611330
Wren McPherson <wren.mcpherson@yahoo.com>
2021-02-11 23:51:33 -0500

    update posts

--- routes/api/users.js
+++ routes/api/users.js
@@ -93,5 +93,15 @@ router.get('/api/users/:userId/follows', asyncHandler(async (req, res, next) => 
     res.json({ users }) 
 }))
 
+// get all user's posts with specific tag
+router.get('/api/users/:userId/tags/:tag', asyncHandler(async (req, res, next) => {
+    const userId = parseInt(req.params.userId, 10);
+    const tag = parseInt(req.params.tag);
+    const posts = Tag.findAll({ where: { description: tag },  })
+    // const user = await User.findByPk(userId, { include: [{ model: Follow }] });
+    // const users = await User.findAll({ where: { id: followIds }, order: [['username', 'DESC']] });
+    res.json({ posts }) 
+}))
+
 
 module.exports = router;
